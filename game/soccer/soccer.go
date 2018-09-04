a3d45d591bb8156f84d7adda7eaf21e869e3642c
Jérôme Schneider <contact@netgusto.com>
2018-09-04 13:52:11 +0200

    Field VIZ specs

--- game/soccer/soccer.go
+++ game/soccer/soccer.go
@@ -1,8 +1,12 @@
 package soccer
 
 import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+
 	"github.com/bytearena/box2d"
-	"github.com/bytearena/core/common/types"
+	commontypes "github.com/bytearena/core/common/types"
 	"github.com/bytearena/core/common/utils/vector"
 	"github.com/bytearena/ecs"
 )
@@ -40,8 +44,47 @@ func NewSoccerGame() *SoccerGame {
 }
 
 func initPhysicalWorld(game *SoccerGame) {
+
 	// Static physical objects
 	// TODO: define outer perimeters (walls) and goals
+
+	points := [][2]float64{
+		[2]float64{0.0, 0.0},
+		[2]float64{0.0, 1050.0},
+		[2]float64{1050.0, 680.0},
+		[2]float64{0, 680.0},
+		[2]float64{0, 0.0},
+	}
+
+	// north terrain boundary
+	bodydef := box2d.MakeB2BodyDef()
+	bodydef.Type = box2d.B2BodyType.B2_staticBody
+
+	body := game.PhysicalWorld.CreateBody(&bodydef)
+	vertices := make([]box2d.B2Vec2, len(points))
+
+	for i := 0; i < len(points); i++ {
+		vertices[i].Set(points[i][0], points[i][1]*-1) // TODO(jerome): invert axes in transform, not here
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("\n\nERROR - Terrain outer boundaries are not valid; perhaps some vertices are duplicated?\n\n")
+			panic(r)
+		}
+	}()
+
+	prev := len(vertices) - 1
+	for cur := 0; cur < len(vertices); cur++ {
+		shape := box2d.MakeB2EdgeShape()
+		shape.Set(vertices[prev], vertices[cur])
+		body.CreateFixture(&shape, 0.0)
+
+		prev = cur
+	}
+
+	spew.Dump(body)
+
 }
 
 func (game SoccerGame) getEntity(id ecs.EntityID, tagelements ...interface{}) *ecs.QueryResult {
@@ -56,19 +99,19 @@ func (game *SoccerGame) Initialize(cbkGameOver func()) {
 	game.cbkGameOver = cbkGameOver
 }
 
-func (game *SoccerGame) Step(ticknum int, dt float64, mutations []types.AgentMutationBatch) {
+func (game *SoccerGame) Step(ticknum int, dt float64, mutations []commontypes.AgentMutationBatch) {
 	game.ticknum = ticknum
 }
 
 func (game *SoccerGame) NewEntityAgent(
-	agent *types.Agent,
+	agent *commontypes.Agent,
 	spawnPosition vector.Vector2, // spawnPosition in physical space; TODO: fix this, should be in agent space
 ) ecs.EntityID {
 	agentEntity := game.manager.NewEntity()
 	return agentEntity.GetID()
 }
 
-func (game *SoccerGame) RemoveEntityAgent(agent *types.Agent) {
+func (game *SoccerGame) RemoveEntityAgent(agent *commontypes.Agent) {
 	qr := game.getEntity(agent.EntityID)
 	game.manager.DisposeEntity(qr)
 }
@@ -82,7 +125,16 @@ func (game *SoccerGame) GetAgentWelcome(entityid ecs.EntityID) []byte {
 }
 
 func (game *SoccerGame) GetVizInitJson() []byte {
-	return []byte("{}")
+	return []byte(
+		`{
+			"field": {
+				"width": 1050,
+				"height": 680,
+				"padding": 60,
+				"goallength": 70
+			}
+		}`,
+	)
 }
 
 func (game *SoccerGame) GetVizFrameJson() []byte {
